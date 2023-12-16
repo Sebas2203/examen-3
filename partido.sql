096bd0fea42b1fd7cce0a02e50a64c94041d01af
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2023-12-15 19:00:05 -0600

    examen 3

---
+++ partido.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE encuestaPartidos
+GO
+
+USE encuestaPartidos
+GO
+
+--creación de tablas
+CREATE TABLE encuesta
+(
+	id INT IDENTITY,
+	nombre VARCHAR(50) NOT NULL,
+	edad INT,
+	correo VARCHAR(50) NOT NULL,
+
+	CONSTRAINT pk_encuesta PRIMARY KEY (id)
+
+)
+GO
+
+CREATE TABLE partido
+(
+	id INT IDENTITY (0,2),
+	idEncuesta INT,
+	partidoPolitico VARCHAR(10) NOT NULL
+
+	CONSTRAINT pk_idPartido PRIMARY KEY (id),
+	CONSTRAINT fk_idEncuesta FOREIGN KEY (idEncuesta) REFERENCES encuesta (id)
+)
+GO
+
+--llenado de tablas 
+CREATE PROCEDURE llenarEncuesta
+	@nombre VARCHAR(50),
+	@edad INT,
+	@correo VARCHAR(50)
+AS
+	BEGIN
+		INSERT INTO encuesta (nombre, edad, correo) VALUES (@nombre, @edad, @correo)
+	END
+GO
+
+EXEC llenarEncuesta 'Sebastian', 20, 'sebas@uh.com'
+
+
+
+
+
+
+
+
+
+--INNERJOINS
